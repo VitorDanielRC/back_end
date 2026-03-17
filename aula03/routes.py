2cb691237ea0e0bd39d891c9458da3aa7ec3f887
Vitor Daniel <143666152+VitorDanielRC@users.noreply.github.com>
2026-03-17 20:20:14 -0300

    routes

---
+++ aula03/routes.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+from database import users_collection
+from schemas import User
+
+router = APIRouter()
+
+@router.get("/users")
+def list_users():
+    users = []
+
+    for user in users_collection.find():
+        user["_id"] = str(user["_id"])
+        users.append(user)
+
+    return users
+
+#POST - CREATE USER
+#GET - USER BY ID
