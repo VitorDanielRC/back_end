7180657d1d77c006e22f42abc731e4b3f93a1634
Vitor Daniel <143666152+VitorDanielRC@users.noreply.github.com>
2026-03-17 20:20:33 -0300

    excluido

--- aula_03/database.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-MONGO_URL = "mongodb://mongo:27018"
-
-client = MongoClient(MONGO_URL)
-
-db = client["aula_nosql"]
-users_collections = db["users"]
--- aula_03/schemas.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class user(BaseModel):
-    username: str
-    email: str
-    password: str
