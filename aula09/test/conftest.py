007b5c7782476e490a5fca23d7a49761ce88a7d1
Vitor Daniel <143666152+VitorDanielRC@users.noreply.github.com>
2026-06-03 21:07:47 -0300

    conftest

--- aula09/test/conftest.py
+++ aula09/test/conftest.py
@@ -1,1 +1,9 @@
-import fastapi 
+import pytest
+import main 
+@pytest.fixture
+def client():
+    main.tarefas_db.clear()
+    main.proximo_id = 1
+    yield TestClient(app)
+    main.tarefas_db.clear()
+    main.proximo_id = 1
